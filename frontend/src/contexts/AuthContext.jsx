3fdf2140efd5d157e98d720e268e026a9af83493
Aung <august810.a@gmail.com>
2025-01-11 01:15:01 +0700

    fixed useAuth

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,57 +1,50 @@
-import { createContext, useEffect, useReducer, useState } from "react";
-import axios from "axios";
+import { createContext, useEffect, useReducer } from "react";
+import axios from 'axios';
 
 const AuthContext = createContext();
 
 let AuthReducer = (state, action) => {
-  switch (action.type) {
-    case "LOGIN":
-      localStorage.setItem("user", JSON.stringify(action.payload));
-      return { user: action.payload };
-    case "LOGOUT":
-      localStorage.removeItem("user");
-      return { user: null };
-    default:
-      return state;
-  }
-};
+    switch (action.type) {
+        case "LOGIN":
+            //store user in localstorage
+            localStorage.setItem('user', JSON.stringify(action.payload))
+            return { user: action.payload };
+        case "LOGOUT":
+            //remove user in localstorage
+            localStorage.removeItem('user');
+            return { user: null };
+        default:
+            return state;
+    }
+}
 
 const AuthContextProvider = ({ children }) => {
-  let [state, dispatch] = useReducer(AuthReducer, {
-    user: null,
-  });
+    let [state, dispatch] = useReducer(AuthReducer, {
+        user: null
+    });
 
-  const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        try {
+            axios.get('/api/users/me').then(res => {
+                let user = res.data;
+                if (user) {
+                    dispatch({ type: 'LOGIN', payload: user })
+                } else {
+                    dispatch({ type: "LOGOUT" });
+                }
+            })
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res = await axios.get("/api/users/me");
-        const user = res.data;
-        if (user) {
-          dispatch({ type: "LOGIN", payload: user });
-        } else {
-          dispatch({ type: "LOGOUT" });
+        } catch (e) {
+            dispatch({ type: "LOGOUT" });
         }
-      } catch (e) {
-        dispatch({ type: "LOGOUT" });
-      } finally {
-        setLoading(false); 
-      }
-    };
+    }, [])
 
-    fetchUser();
-  }, []);
+    return (
+        <AuthContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
 
-  if (loading) {
-    return <div>Loading...</div>; // Or a loading spinner
-  }
-
-  return (
-    <AuthContext.Provider value={{ ...state, dispatch }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
 
 export { AuthContext, AuthContextProvider };
