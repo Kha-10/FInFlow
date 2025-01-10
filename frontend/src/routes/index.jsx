c7ae834f30f53db704cef49438d4e973ae5c4a0b
Aung <august810.a@gmail.com>
2025-01-10 23:17:41 +0700

    fixed routes in frontend

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -14,7 +14,7 @@ import { AuthContext } from "@/contexts/AuthContext";
 
 function index() {
   let { user } = useContext(AuthContext);
-
+    console.log('user',user);
   const router = createBrowserRouter([
     {
       path: "/",
@@ -22,23 +22,23 @@ function index() {
       children: [
         {
           path: "/dashboard",
-          element: user ? <Home /> : <Navigate to={"/login"} />,
-        //   element:  <Home /> ,
+        //   element: user ? <Home /> : <Navigate to={"/login"} />,
+            element:  <Home /> ,
         },
         {
           path: "/purchases",
-          element: user ? <AddToPurchases /> : <Navigate to={"/login"} />,
-        //   element:  <AddToPurchases /> ,
+        //   element: user ? <AddToPurchases /> : <Navigate to={"/login"} />,
+            element:  <AddToPurchases /> ,
         },
         {
           path: "/login",
-          element: !user ? <LogIn /> : <Navigate to={"/dashboard"} />,
-        //   element: <LogIn /> ,
+          //   element: !user ? <LogIn /> : <Navigate to={"/dashboard"} />,
+          element: <LogIn />,
         },
         {
           path: "/register",
-          element: !user ? <Register /> : <Navigate to={"/dashboard"} />,
-        //   element:  <Register /> ,
+          //   element: !user ? <Register /> : <Navigate to={"/dashboard"} />,
+          element: <Register />,
         },
       ],
     },
